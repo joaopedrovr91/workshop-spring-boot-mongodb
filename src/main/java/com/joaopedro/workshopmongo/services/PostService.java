46d10addea7c3b60fb3086081ad7cabb3fbc3f3c
joaopedrovr91 <joaopedrovr91@gmail.com>
2024-03-15 12:15:39 -0300

    obtendo um post por id

---
+++ src/main/java/com/joaopedro/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.joaopedro.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.joaopedro.workshopmongo.domain.Post;
+import com.joaopedro.workshopmongo.repository.PostRepository;
+import com.joaopedro.workshopmongo.services.exception.objectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new objectNotFoundException("Objeto não encontrado"));
+	}
+
+}
